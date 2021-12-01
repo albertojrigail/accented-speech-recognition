d8eeba9811801e7bf0d684efcf60f249f8716ac9
Alberto Rigail <albertojrigail@hotmail.com>
2021-12-01 15:38:15 -0500

    Process WER scores data

--- edit_csv.py
+++ edit_csv.py
@@ -1,17 +1,20 @@
 from csv import reader
 from os.path import join
 import pandas as pd
+import speech_accent_archive_analysis
 
-files = ["amazon_transcribe_old.csv", "google_speech_to_text_old.csv", "mozilla_deepspeech.csv"]
+files = ["amazon_transcribe.csv", "google_speech_to_text.csv", "mozilla_deepspeech.csv"]
+output_files = ["amazon_transcribe_key.csv", "google_speech_to_text_key.csv", "mozilla_deepspeech_key.csv"]
 scores_dir = "asr_scores"
 
-for f in files:
+for i in range(len(files)):
+    f = files[i]
     path = join(scores_dir, f)
     df = pd.read_csv(path)
-    for i in range(len(df['number'])):
-        df['number'][i] = i
-    print(df['number'])
-    df.to_csv(path, index=False)
+    df = df[df['frequency'] > 15]
+    output_f = output_files[i]
+    output_path = join(scores_dir, output_f)
+    df.to_csv(output_path, index=False)
 
 
 
